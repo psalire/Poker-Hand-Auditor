724460cd31c236ba494018ed6b1612a49586630a
Philip <psalire@ucsc.edu>
2020-12-18 16:45:37 -0800

    add summary section, move output to Results.py

---
+++ Results.py
@@ -0,0 +1,196 @@
+from scipy.stats import chisquare
+from math import sqrt
+
+class Results:
+
+    def __init__(self, label_column_size=15, value_column_size=15, columns=6):
+        self.set_label_column_size(label_column_size)
+        self.set_value_column_size(value_column_size)
+        self._set_full_width(6)
+
+    # Set sizes and formatting values based on size
+    def set_label_column_size(self, size):
+        self._label_column_size = size
+        self._label_column = '{:^%d}|' % self._label_column_size
+    def set_value_column_size(self, size):
+        # Various column formats
+        self._value_column_size = size
+        self._value_column = '{:^%d}|' % self._value_column_size
+        self._float_value_column = '{:^%df}|' % self._value_column_size
+        self._float_value = '{:^%df}' % self._value_column_size
+    def _set_full_width(self, columns):
+        # Various column formats
+        self._full_width = self._label_column_size + self._value_column_size*columns + columns
+        self._horizontal_divider = ('{:-^%d}' % self._full_width).format('')
+        self._value_span_fullwidth = '{:^%d}|' % self._full_width
+        self._float_value_span_halfwidth = '{:^%df}' % (self._full_width // 2)
+        self._value_column_span_halfwidth = '{:^%d}|' % (self._full_width // 2)
+
+        # Row formats
+        self._results_row = self._label_column + self._value_column*columns
+        self._halfwidth_value_span_row = self._value_column_span_halfwidth*2
+        self._totals_row = self._label_column + self._float_value_column + self._value_column + self._float_value_column*(columns-3) + self._value_column
+
+    # Printing rows
+    def _print_row(self, s, divider=False):
+        print(s)
+        if divider:
+            self._print_horizontal_divider()
+    def _print_results_row(self, *argv, divider=False):
+        self._print_row(self._results_row.format(*argv), divider)
+    def _print_halfwidth_value_span_row(self, *argv, divider=False):
+        self._print_row(self._halfwidth_value_span_row.format(*argv), divider)
+    def _print_fullwidth_value_span_row(self, *argv, divider=False):
+        self._print_row(self._value_span_fullwidth.format(*argv), divider)
+    # Totals row used in calculate_and_print_results
+    def _print_totals_row(self, *argv, divider=False):
+        self._print_row(self._totals_row.format(*argv), divider)
+
+    def _format_if_valid(self, format_str, val, append=''):
+        return format_str.format(val) if val != None else str(val)+append
+    def _print_horizontal_divider(self):
+        print(self._horizontal_divider)
+    def _print_string_with_divider(self, s):
+        print(s)
+        self._print_horizontal_divider()
+
+    # Print table of results
+    ## title: str, title of the table
+    ## label: str, label of the first column
+    ## expected: dict, expected values
+    ## sample: dict, sampled values
+    ## std_dev: int 1,2,3, std_dev for use in confidence limit
+    ## label_column_size: int, width of first column
+    ## value_column_size: int, width of other columns
+    ## is_normal: bool, is normally distributed, whether to calculate confidence intervals
+    def calculate_and_print_results(self, title, label, expected, sample, summary, test_results, std_dev=2, is_normal=True):
+        columns = 6 if is_normal else 4
+        self._set_full_width(columns)
+
+        sample_size = sum(sample.values())
+
+        # Print title and column headers
+        self._print_string_with_divider('\n')
+        if is_normal:
+            confidence_limit = ['68', '95', '99.7'][std_dev-1]
+            table_title = '{}, {}% Confidence Level, n={}'.format(title, confidence_limit, sample_size)
+            column_name_args = (label, 'Expected', 'Expected Size','Sample', 'Lower', 'Upper', 'Sample Size')
+            # Track proportion values as list of tuples (sample,lower,upper)
+            proportions = []
+        else:
+            table_title = '{}, n={}'.format(title, sample_size)
+            column_name_args = (label, 'Expected', 'Expected Size','Sample', 'Sample Size')
+        self._print_fullwidth_value_span_row(table_title, divider=True)
+        self._print_results_row(*column_name_args, divider=True)
+
+        totals = [0 for _ in range(columns)]
+        expected_sizes = []
+
+        # Print column values
+        for key in sample:
+            sample_percentage = sample[key]/sample_size
+            expected_size = round(expected[key]*sample_size)
+            expected_sizes.append(expected_size)
+
+            # Print row of values
+            if is_normal:
+                # Calculate standard error
+                ## can't divide by zero
+                if sample[key] != 0:
+                    standard_error = sqrt((expected[key] * (1-expected[key])) / sample[key])
+                    lower_percentage = expected[key] - std_dev*standard_error
+                    upper_percentage = expected[key] + std_dev*standard_error
+                    proportions.append((sample_percentage, lower_percentage, upper_percentage))
+                else:
+                    standard_error = None
+                    lower_percentage = None
+                    upper_percentage = None
+
+                self._print_results_row(
+                    key, # Label
+                    self._format_if_valid(self._float_value, expected[key]), # Expected proportion
+                    expected_size, # Expected size
+                    self._format_if_valid(self._float_value, sample_percentage), # Sample proportion
+                    self._format_if_valid(self._float_value, lower_percentage), # Upper confidence limit
+                    self._format_if_valid(self._float_value, upper_percentage), # Lower confidence limit
+                    sample[key], # Sample size
+                )
+            else:
+                self._print_results_row(
+                    key, # Label
+                    self._format_if_valid(self._float_value, expected[key]), # Expected proportion
+                    expected_size, # Expected size
+                    self._format_if_valid(self._float_value, sample_percentage), # Sample proportion
+                    sample[key], # Sample size
+                )
+
+
+            # Count totals
+            totals[0] += expected[key]
+            if is_normal:
+                totals[1] += expected_size
+                if standard_error:
+                    totals[2] += sample_percentage
+                    totals[3] += lower_percentage
+                    totals[4] += upper_percentage
+                totals[5] += sample[key]
+            else:
+                totals[1] += expected_size
+                totals[2] += sample_percentage
+                totals[3] += sample[key]
+        self._print_horizontal_divider()
+        self._print_totals_row('Total', *(total for total in totals), divider=True)
+
+        # Find and print chi-square values
+        if 0 not in expected_sizes:
+            chi_square, chi_square_pvalue = chisquare(list(sample.values()), f_exp=expected_sizes)
+        else:
+            chi_square, chi_square_pvalue = None, None
+        self._print_fullwidth_value_span_row('Chi-Square Goodness of Fit Test Results', divider=True)
+        self._print_halfwidth_value_span_row(
+            'Chi-square',
+            self._format_if_valid(self._float_value_span_halfwidth, chi_square, ' (Expected value(s) == 0)'),
+            divider=False,
+        )
+        self._print_halfwidth_value_span_row(
+            'p-value',
+            self._format_if_valid(self._float_value_span_halfwidth, chi_square_pvalue, ' (Expected value(s) == 0)'),
+            divider=True,
+        )
+
+        assert sample_size == totals[5 if is_normal else 3] # Sanity check for sample size
+
+        # Write summary of results
+        summary.append(('{}, n={}'.format(title, sample_size), []))
+        if is_normal:
+            test_results.append(all([l < s < u for s,l,u in proportions]))
+            summary[-1][1].append((
+                'Sample in {}% confidence interval'.format(confidence_limit),
+                'PASS' if test_results[-1] else 'FAIL',
+            ))
+        if chi_square_pvalue != None:
+            test_results.append(chi_square_pvalue > 0.05)
+            summary[-1][1].append((
+                'Chi-square p-value > 0.05',
+                'PASS' if test_results[-1] else 'FAIL',
+            ))
+
+    # Print summary
+    ## summary: list of pair of strs
+    ## test_results: list of bool
+    def print_summary(self, summary, test_results):
+        self._set_full_width(4)
+
+        self._print_string_with_divider('')
+        self._print_fullwidth_value_span_row('SUMMARY', divider=True)
+        for t,d in summary:
+            self._print_fullwidth_value_span_row(t, divider=True)
+            self._print_halfwidth_value_span_row('Test', 'Result', divider=True)
+            for test, result in d:
+                self._print_halfwidth_value_span_row(test, result)
+            self._print_horizontal_divider()
+        passing_test_count = test_results.count(True)
+        self._print_fullwidth_value_span_row(
+            'Passing Tests: {}/{} ({:.2f}%)'.format(passing_test_count, len(test_results), 100*passing_test_count/len(test_results)),
+            divider=True,
+        )
--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import os
 import re
 import argparse
 import Parse
+from Results import Results
 from treys import Card, Evaluator
 from itertools import combinations
-from scipy.stats import chisquare
-from math import sqrt
 
 CARDS = [
     '2c','2d','2h','2s',
@@ -22,130 +21,6 @@ CARDS = [
     'Kc','Kd','Kh','Ks',
     'Ac','Ad','Ah','As',
 ]
-
-# Helper functions for calculate_and_print_results
-def format_if_valid(format_str, val, append=''):
-    return format_str.format(val) if val != None else str(val)+append
-
-# Print table of results
-## title: str, title of the table
-## label: str, label of the first column
-## expected: dict, expected values
-## sample: dict, sampled values
-## std_dev: int 1,2,3, std_dev for use in confidence limit
-## label_column_size: int, width of first column
-## value_column_size: int, width of other columns
-## is_normal: bool, is normally distributed, whether to calculate confidence intervals
-def calculate_and_print_results(title, label, expected, sample, std_dev=2, label_column_size=15, value_column_size=15, is_normal=True):
-    columns = 6 if is_normal else 4
-    full_width = label_column_size + value_column_size*columns + columns
-    horizontal_divider = ('{:-^%d}' % full_width).format('')
-
-    def print_with_divider(string):
-        print(string)
-        print(horizontal_divider)
-
-    # Format string based on number and size of columns
-    float_value = '{:^%df}' % value_column_size
-    float_value_span_halfwidth = '{:^%df}' % (full_width // 2)
-    ## Columns
-    label_column = '{:^%d}|' % label_column_size
-    value_column = '{:^%d}|' % value_column_size
-    float_value_column = '{:^%df}|' % value_column_size
-    value_column_span_halfwidth = '{:^%d}|' % (full_width // 2)
-    value_span_fullwidth = '{:^%d}|' % full_width
-    ## Row formatting
-    results_row = label_column + value_column*columns
-    totals_row = label_column + float_value_column + value_column + float_value_column*(columns-3) + value_column
-    chi_square_row = value_column_span_halfwidth*2
-
-    sample_size = sum(sample.values())
-
-    # Print title and column headers
-    print('\n'+horizontal_divider)
-    if is_normal:
-        confidence_limit = ['68', '95', '99.7'][std_dev-1]
-        title = '{}, {}% Confidence Level, n={}'.format(title, confidence_limit, sample_size)
-        column_name_args = (label, 'Expected', 'Expected Size','Sample', 'Lower', 'Upper', 'Sample Size')
-    else:
-        title = '{}, n={}'.format(title, sample_size)
-        column_name_args = (label, 'Expected', 'Expected Size','Sample', 'Sample Size')
-    print_with_divider(value_span_fullwidth.format(title))
-    print_with_divider(results_row.format(*column_name_args))
-
-    totals = [0 for _ in range(columns)]
-    expected_sizes = []
-
-    # Print column values
-    for key in sample:
-        sample_percentage = sample[key]/sample_size
-        expected_size = round(expected[key]*sample_size)
-        expected_sizes.append(expected_size)
-
-        # Print row of values
-        if is_normal:
-            # Calculate standard error
-            ## can't divide by zero
-            if sample[key] != 0:
-                standard_error = sqrt((expected[key] * (1-expected[key])) / sample[key])
-                lower_percentage = expected[key] - std_dev*standard_error
-                upper_percentage = expected[key] + std_dev*standard_error
-            else:
-                standard_error = None
-                lower_percentage = None
-                upper_percentage = None
-
-            print(results_row.format(
-                    key, # Label
-                    format_if_valid(float_value, expected[key]), # Expected proportion
-                    expected_size, # Expected size
-                    format_if_valid(float_value, sample_percentage), # Sample proportion
-                    format_if_valid(float_value, lower_percentage), # Upper confidence limit
-                    format_if_valid(float_value, upper_percentage), # Lower confidence limit
-                    sample[key], # Sample size
-                )
-            )
-        else:
-            print(results_row.format(
-                    key, # Label
-                    format_if_valid(float_value, expected[key]), # Expected proportion
-                    expected_size, # Expected size
-                    format_if_valid(float_value, sample_percentage), # Sample proportion
-                    sample[key], # Sample size
-                )
-            )
-        # Count totals
-        totals[0] += expected[key]
-        if is_normal:
-            totals[1] += expected_size
-            if standard_error:
-                totals[2] += sample_percentage
-                totals[3] += lower_percentage
-                totals[4] += upper_percentage
-            totals[5] += sample[key]
-        else:
-            totals[1] += expected_size
-            totals[2] += sample_percentage
-            totals[3] += sample[key]
-    print(horizontal_divider)
-    print_with_divider(totals_row.format('Total', *(total for total in totals)))
-
-    # Find and print chi-square values
-    if 0 not in expected_sizes:
-        chi_square, chi_square_pvalue = chisquare(list(sample.values()), f_exp=expected_sizes)
-    else:
-        chi_square, chi_square_pvalue = None, None
-    print_with_divider(value_span_fullwidth.format('Chi-Square Goodness of Fit Test Results'))
-    print(chi_square_row.format(
-        'Chi-square',
-        format_if_valid(float_value_span_halfwidth, chi_square, ' (An expected value == 0)'),
-    ))
-    print_with_divider(chi_square_row.format(
-        'p-value',
-        format_if_valid(float_value_span_halfwidth, chi_square_pvalue, ' (An expected value == 0)'),
-    ))
-
-    assert sample_size == totals[5 if is_normal else 3] # Sanity check for sample size
 
 # Helper function for counting hole cards
 ## hole_cards: tuple, pair of hole cards
@@ -248,27 +123,38 @@ def main():
                     for hand in combinations(list(hc)+board, 5):
                         count_hand_frequency(hand[:2], hand[2:], evaluator, hand_allcombinations_frequency)
 
+    summary = [] # List of strs of result summaries
+    test_results = [] # List of bool of pass/fail test results
+
+    results = Results()
+
     # Print all results
-    calculate_and_print_results(
+    results.calculate_and_print_results(
         'Distribution of Hands',
         'Hand',
         hand_probabilites,
         hand_frequency,
+        summary,
+        test_results,
         std_dev=args.stdev,
     )
     if args.allcombinations:
-        calculate_and_print_results(
+        results.calculate_and_print_results(
             'Distribution of All Hand Combinations',
             'Hand',
             hand_probabilites,
             hand_allcombinations_frequency,
+            summary,
+            test_results,
             std_dev=args.stdev,
         )
-    calculate_and_print_results(
+    results.calculate_and_print_results(
         'Distribution of Cards',
         'Card',
         {x: 1/len(CARDS) for x in CARDS},
         card_frequency,
+        summary,
+        test_results,
         std_dev=args.stdev,
         is_normal=False
     )
@@ -276,11 +162,13 @@ def main():
         hole_card_combinations = [' '.join(x) for x in combinations(CARDS, 2)]
         hole_card_expected_frequency = {x: hole_card_combinations.count(x) / len(hole_card_combinations) for x in hole_card_combinations}
 
-        calculate_and_print_results(
+        results.calculate_and_print_results(
             'Distribution of Hole Cards with suits',
             'Hole Cards',
             hole_card_expected_frequency,
             hole_card_frequency,
+            summary,
+            test_results,
             std_dev=args.stdev,
             is_normal=False,
         )
@@ -288,14 +176,18 @@ def main():
         hole_card_nosuit_combinations = [' '.join((x[0], y[0])) for x, y in combinations(CARDS, 2)]
         hole_card_nosuits_expected_frequency = {x: hole_card_nosuit_combinations.count(x) / len(hole_card_nosuit_combinations) for x in hole_card_nosuit_combinations}
 
-        calculate_and_print_results(
+        results.calculate_and_print_results(
             'Distribution of Hole Cards without suits',
             'Hole Cards',
             hole_card_nosuits_expected_frequency,
             hole_card_nosuits_frequency,
+            summary,
+            test_results,
             std_dev=args.stdev,
             is_normal=False,
         )
+
+    results.print_summary(summary, test_results)
 
 if __name__ == '__main__':
     main()
